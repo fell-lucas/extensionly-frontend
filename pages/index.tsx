720e9e2bfda2f26036691b76f90c3ada9fabc132
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2022-06-16 20:51:01 -0300

    chore: initialize repository with nextjs project

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Extensionly</title>
+        <meta name='description' content='Extensionly app' />
+      </Head>
+
+      <main>
+        <h1>Extensionly</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
